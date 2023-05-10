a795c1a8b3720b9843ddd1233e00352d5b142671
Lucas Luna <lucasluna8926@gmail.com>
2023-05-10 04:08:34 -0300

    ordenar codigo css

--- main.js
+++ main.js
@@ -20,23 +20,9 @@
     $(window).on("scroll", toggleCondorClass);
 
 
-    // guardar contenido de menú hamburguesa cuando se hace scroll
-    window.addEventListener('scroll', function() {
-      var navbarToggle = document.querySelector('.navbar-toggler');
-      var ariaExpanded = navbarToggle.getAttribute('aria-expanded');
-      var navbarNav = document.querySelector('#navbarNav');
-      var scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+  
     
-      if (scrollPosition > 0 && ariaExpanded === 'true') {
-        navbarToggle.setAttribute('aria-expanded', 'false');
-        navbarToggle.classList.add('collapsed');
-        navbarNav.classList.remove('show');
-      } else if (scrollPosition === 0 && ariaExpanded === 'false') {
-        navbarToggle.setAttribute('aria-expanded', 'true');
-        navbarToggle.classList.remove('collapsed');
-      }
-    });
-    
+   
     
 
     
